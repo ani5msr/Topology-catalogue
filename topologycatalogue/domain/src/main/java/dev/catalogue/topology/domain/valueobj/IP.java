74ed5831f568312277dc4ba71c32bcd4422dac60
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-22 13:14:30 +0530

    Added IP class for checking address and assigning protocol

---
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/valueobj/IP.java
@@ -0,0 +1,28 @@
+package dev.catalogue.topology.domain.valueobj;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class IP {
+	 private String ipAddress;
+	 private Protocol protocol;
+     public IP(String ipAddress){
+	      if(ipAddress == null)
+	          throw new IllegalArgumentException("Null IP address");
+	      this.ipAddress = ipAddress;
+	      if(ipAddress.length()<=15) {
+	          this.protocol = Protocol.IPV4;
+	      } else {
+	        this.protocol = Protocol.IPV6;
+	      }
+	    }
+	 public static IP fromAddress(String ipAddress){
+	        return new IP(ipAddress);
+	    }
+}
