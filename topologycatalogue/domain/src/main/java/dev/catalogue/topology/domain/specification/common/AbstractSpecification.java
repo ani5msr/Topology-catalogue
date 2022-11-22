19785a65d7f53a77667969c871ac51bb0ab91e07
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-22 16:30:46 +0530

    Added abstract function and to check two routers specifications

--- topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/specification/common/AbstractSpecification.java
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/specification/common/AbstractSpecification.java
@@ -2,4 +2,7 @@ package dev.catalogue.topology.domain.specification.common;
 
 public abstract class AbstractSpecification<T> implements Specification<T>{
 		public abstract boolean isSatisfiedBy(T t);
+		public Specification<T> and(final Specification<T> specification) {
+	        return new AndSpecification<T>(this, specification);
+	    }
 }
