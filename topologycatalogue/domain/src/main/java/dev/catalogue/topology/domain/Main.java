e4547edac9b527aded75d2f988eb6bd633a3c010
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-22 12:23:51 +0530

    Reconfigured project

---
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/Main.java
@@ -0,0 +1,8 @@
+package dev.catalogue.topology.domain;
+
+public class Main {
+
+  public String format(String input) {
+    return input.toUpperCase();
+  }
+}
---
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/valueobj/ID.java
@@ -0,0 +1,21 @@
+package dev.catalogue.topology.domain.valueobj;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import java.util.UUID;
+@Getter
+@ToString
+@EqualsAndHashCode
+public class ID {
+	private final UUID id;
+	 private ID(UUID id){
+	 this.id = id;
+	 }
+	 public static ID withId(String id){
+	 return new ID(UUID.fromString(id));
+	 }
+	 public static ID withoutId(){
+	 return new ID(UUID.randomUUID());
+	 }
+
+}
