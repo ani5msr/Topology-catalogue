579c5a91412fb7f0a48d05f3d7b286705d0ee5f9
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-22 15:13:45 +0530

    Added constructor to Equipment entity

--- topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/entity/Equipment.java
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/entity/Equipment.java
@@ -5,19 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import dev.catalogue.topology.domain.valueobj.*;
 @Getter
-
+@AllArgsConstructor
 public class Equipment {
 	protected ID id;
 	protected Vendor vendor;
 	protected Model model;
 	protected IP ip;
 	protected Location location;
+	public Equipment(ID id, Vendor vendor, Model model, IP ip, Location location) {
+		this.id = id;
+		this.vendor = vendor;
+		this.model = model;
+		this.ip = ip;
+		this.location = location;
+	}
 	public static Predicate<Equipment>
 	 getVendorPredicate(Vendor vendor){
 	 return r -> r.getVendor().equals(vendor);
 	 }
 	protected Vendor getVendor() {
 		return this.vendor;
+	}
+	public Model getModel() {
+		return this.model;
 	}
 
 }
