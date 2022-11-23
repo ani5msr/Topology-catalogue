e21468532aac40529a6831b182ca0032b3bff317
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-23 17:30:37 +0530

    Added Network service for fetching network by predicates

---
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/service/Networkservice.java
@@ -0,0 +1,16 @@
+package dev.catalogue.topology.domain.service;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import dev.catalogue.topology.domain.valueobj.Network;
+
+public class Networkservice {
+	 public static List<Network> filterAndRetrieveNetworks(List<Network> networks, Predicate<Network> networkPredicate){
+	        return networks
+	                .stream()
+	                .filter(networkPredicate)
+	                .collect(Collectors.<Network>toList());
+	    }
+}
