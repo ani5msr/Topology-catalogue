df35b8f157ec5037edb27ba646a8ce0f4155d54a
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-23 12:03:07 +0530

    Added check exception method

--- topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/specification/common/AbstractSpecification.java
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/specification/common/AbstractSpecification.java
@@ -1,9 +1,9 @@
 package dev.catalogue.topology.domain.specification.common;
-
+import dev.catalogue.topology.domain.exception.*;
 public abstract class AbstractSpecification<T> implements Specification<T>{
 		public abstract boolean isSatisfiedBy(T t);
 		public Specification<T> and(final Specification<T> specification) {
 	        return new AndSpecification<T>(this, specification);
 	    }
-		
+		public abstract void check(T t) throws GenericException;
 }
