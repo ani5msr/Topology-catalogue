677ee406277f875b8e17fb3328f8ff6009a183b7
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-23 16:00:13 +0530

    Added Edge Router entity for handling edge routers

---
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/entity/EdgeRouter.java
@@ -0,0 +1,29 @@
+package dev.catalogue.topology.domain.entity;
+
+import java.util.Map;
+import dev.catalogue.topology.domain.entity.*;
+import dev.catalogue.topology.domain.valueobj.*;
+import dev.catalogue.topology.domain.specification.*;
+import lombok.Builder;
+
+public class EdgeRouter extends Router {
+	private Map<ID, Switch> switches;
+    @Builder
+	public EdgeRouter(ID id, Vendor vendor, Model model, IP ip, Location location, Routertype routerType, Map<ID, Switch> switches) {
+	        super(id, vendor, model, ip, location, routerType);
+	        this.switches = switches;
+	    }
+    public void addSwitch(Switch anySwitch) {
+        var sameCountryRouterSpec = new SameCountrySpecification(this);
+        var sameIpSpec = new SameIPSpecification(this);
+        sameCountryRouterSpec.check(anySwitch);
+        sameIpSpec.check(anySwitch);
+        this.switches.put(anySwitch.id,anySwitch);
+    }
+    public Switch removeSwitch(Switch anySwitch) {
+        var emptyNetworkSpec = new EmptyNetworkSpecification();
+        emptyNetworkSpec.check(anySwitch);
+
+        return this.switches.remove(anySwitch.id);
+    }
+}
