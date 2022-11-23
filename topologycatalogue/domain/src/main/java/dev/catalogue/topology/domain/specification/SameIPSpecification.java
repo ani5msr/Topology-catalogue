3c6c3a663f9038cc77941ecd837117f88c0eedf0
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-23 12:02:09 +0530

    Added SameIp for checking two routers with same IP

---
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/specification/SameIPSpecification.java
@@ -0,0 +1,24 @@
+package dev.catalogue.topology.domain.specification;
+import dev.catalogue.topology.domain.specification.common.*;
+import dev.catalogue.topology.domain.entity.*;
+import dev.catalogue.topology.domain.exception.*;
+public class SameIPSpecification extends AbstractSpecification<Equipment> {
+		private Equipment equipment;
+
+		public Equipment getEquipment() {
+			return equipment;
+		}
+
+		public void setEquipment(Equipment equipment) {
+			this.equipment = equipment;
+		}
+		@Override
+		public boolean isSatisfiedBy(Equipment anyEquipment) {
+	        return !equipment.getIp().equals(anyEquipment.getIp());
+	    }
+	    @Override
+		public void check(Equipment equipment) {
+		        if(!isSatisfiedBy(equipment))
+		            throw new GenericException("It's not possible to attach routers with the same IP");
+		}
+}
