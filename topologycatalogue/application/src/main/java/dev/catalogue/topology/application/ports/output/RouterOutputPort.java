278ab64376b78b247699a897c9283c8cfa4ff65b
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-26 17:25:20 +0530

    Added router output port interface for retrieving and persisting router

---
+++ topologycatalogue/application/src/main/java/dev/catalogue/topology/application/ports/output/RouterOutputPort.java
@@ -0,0 +1,9 @@
+package dev.catalogue.topology.application.ports.output;
+
+import dev.catalogue.topology.domain.entity.*;
+import dev.catalogue.topology.domain.valueobj.*;
+public interface RouterOutputPort {
+	 Router retrieveRouter(ID id);
+
+	 Router persistRouter(Router router);
+}
