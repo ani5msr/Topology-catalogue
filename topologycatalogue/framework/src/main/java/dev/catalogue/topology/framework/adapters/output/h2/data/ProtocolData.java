121557f6c18e2e4349299c20c8f09692163619d7
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-27 22:52:51 +0530

    Added Embeddable entity Protocol class

---
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/output/h2/data/ProtocolData.java
@@ -0,0 +1,9 @@
+package dev.catalogue.topology.framework.adapters.output.h2.data;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public enum ProtocolData {
+    IPV4,
+    IPV6;
+} 
--- topologycatalogue/framework/src/test/java/dev/catalogue/topology/framework/MainTest.java
+++ topologycatalogue/framework/src/test/java/dev/catalogue/topology/framework/MainTest.java
@@ -3,3 +3,20 @@ package dev.catalogue.topology.framework;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+class MainTest {
+
+  private Main cut;
+
+  @BeforeEach
+  void setUp() {
+    this.cut = new Main();
+  }
+
+  @Test
+  void firstTest() {
+    String input = "duke";
+
+    String result = cut.format(input);
+  }
+}
