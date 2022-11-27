8f523e610a2c3206d601de09529c8c5f5c5d3bbc
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-27 23:15:02 +0530

    Created Embeddable entity IpData for network address and protocol

---
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/output/h2/data/IpData.java
@@ -0,0 +1,35 @@
+package dev.catalogue.topology.framework.adapters.output.h2.data;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class IpData {
+	 private String address;
+
+	 @Enumerated(EnumType.STRING)
+	 @Embedded
+	 private ProtocolData protocol;
+	 private IpData(String address){
+	        if(address == null)
+	            throw new IllegalArgumentException("Null IP address");
+	        this.address = address;
+	        if(address.length()<=15) {
+	            this.protocol = ProtocolData.IPV4;
+	        } else {
+	            this.protocol = ProtocolData.IPV6;
+	        }
+	    }
+
+	 public IpData() {
+
+	    }
+
+	 public static IpData fromAddress(String address){
+	        return new IpData(address);
+	    }
+}
