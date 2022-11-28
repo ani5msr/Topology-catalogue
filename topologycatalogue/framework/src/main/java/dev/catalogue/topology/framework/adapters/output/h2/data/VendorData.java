7a4781d788b9da6b2c00045b7c06316a59ce8f54
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-28 11:52:17 +0530

    Added Vendor data embeddable entity for persistence

---
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/output/h2/data/VendorData.java
@@ -0,0 +1,12 @@
+package dev.catalogue.topology.framework.adapters.output.h2.data;
+
+import jakarta.persistence.*;
+@Embeddable
+public enum VendorData {
+	 CISCO,
+	    NETGEAR,
+	    HP,
+	    TPLINK,
+	    DLINK,
+	    JUNIPER
+}
