023a1a5a4ba8eba358264c0f1bc42158827f624f
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-29 12:52:31 +0530

    Added predicate to get network protocol and network name

--- topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/valueobj/Network.java
+++ topologycatalogue/domain/src/main/java/dev/catalogue/topology/domain/valueobj/Network.java
@@ -1,4 +1,6 @@
 package dev.catalogue.topology.domain.valueobj;
+import java.util.function.Predicate;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,6 +13,13 @@ public class Network {
 	private IP networkAddress;
     private String networkName;
 	private int networkCidr;
+    public static Predicate<Network> getNetworkProtocolPredicate(Protocol protocol){
+	        return s -> s.getNetworkAddress().getProtocol().equals(protocol);
+	    }
+
+	public static Predicate<Network> getNetworkNamePredicate(String name){
+	        return s -> s.getNetworkName().equals(name);
+	    }
 	public Network(IP networkAddress,String networkName, int networkCidr){
 			 if(networkCidr <1 || networkCidr>32){
 			 throw new IllegalArgumentException(
