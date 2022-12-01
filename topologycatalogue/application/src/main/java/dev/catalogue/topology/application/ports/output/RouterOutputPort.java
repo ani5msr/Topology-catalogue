16c873c8700888f84636ef1d29cb0e39d25b5ab0
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-01 13:23:30 +0530

    Created remove router method in interface

--- topologycatalogue/application/src/main/java/dev/catalogue/topology/application/ports/output/RouterOutputPort.java
+++ topologycatalogue/application/src/main/java/dev/catalogue/topology/application/ports/output/RouterOutputPort.java
@@ -4,6 +4,6 @@ import dev.catalogue.topology.domain.entity.*;
 import dev.catalogue.topology.domain.valueobj.*;
 public interface RouterOutputPort {
 	 Router retrieveRouter(ID id);
-
 	 Router persistRouter(Router router);
+	 Router removeRouter(ID id);
 }
