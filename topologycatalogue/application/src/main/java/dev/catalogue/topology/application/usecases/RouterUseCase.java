ce67ac10cd0b1cef0ee69a10b85d495df342e03c
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-01 16:02:24 +0530

    Added setOutport method to usecase interface

--- topologycatalogue/application/src/main/java/dev/catalogue/topology/application/usecases/RouterUseCase.java
+++ topologycatalogue/application/src/main/java/dev/catalogue/topology/application/usecases/RouterUseCase.java
@@ -1,8 +1,10 @@
 package dev.catalogue.topology.application.usecases;
 
+import dev.catalogue.topology.application.ports.output.RouterOutputPort;
 import dev.catalogue.topology.domain.entity.*;
 import dev.catalogue.topology.domain.valueobj.*;
 public interface RouterUseCase {
+	 void setOutputPort(RouterOutputPort routerOutputPort);
 	Router createRouter(Vendor vendor,
             Model model,
             IP ip,
