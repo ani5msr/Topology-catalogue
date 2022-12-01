ce06d39803816326bfe134edc184b4f019688c41
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-01 15:42:42 +0530

    Added dependency in application module

--- topologycatalogue/application/src/main/java/module-info.java
+++ topologycatalogue/application/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module application {
 	exports dev.catalogue.topology.application;
 	exports dev.catalogue.topology.application.ports.output;
+	exports dev.catalogue.topology.application.ports.input;
+	exports dev.catalogue.topology.application.usecases;
     requires transitive domain;
     requires static lombok;
 	requires org.junit.jupiter.api;
