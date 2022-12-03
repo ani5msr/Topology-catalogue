ad3e77534c755fde107e2823f9c3bc402065e7a7
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-03 21:56:02 +0530

    Added Quarkus dependency for Rest Services

--- topologycatalogue/framework/src/main/java/module-info.java
+++ topologycatalogue/framework/src/main/java/module-info.java
@@ -5,5 +5,9 @@ module framework {
 	requires org.junit.jupiter.api;
 	requires lombok;
 	requires jakarta.persistence;
+	requires quarkus.hibernate.reactive.panache;
+	requires smallrye.common.annotation;
 	requires org.eclipse.persistence.core;
+	requires java.transaction;
+	requires java.sql;
 }
