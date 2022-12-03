87b6cf5d2de7cdc7a979e452ad04e4c4e0244b9f
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-03 17:42:06 +0530

    Enabled dependency inversion for usecases in application with input port

--- topologycatalogue/application/src/main/java/module-info.java
+++ topologycatalogue/application/src/main/java/module-info.java
@@ -7,4 +7,11 @@ module application {
     requires static lombok;
 	requires org.junit.jupiter.api;
 	requires javaee.web.api;
+	
+	provides dev.catalogue.topology.application.usecases.RouterUseCase with 
+	dev.catalogue.topology.application.ports.input.RouterInputPort;
+	provides dev.catalogue.topology.application.usecases.SwitchUseCase with
+	dev.catalogue.topology.application.ports.input.SwitchInputPort;
+	provides dev.catalogue.topology.application.usecases.NetworkUseCase with
+	dev.catalogue.topology.application.ports.input.NetworkInputPort;
 }
