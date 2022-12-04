350c7d984f932783f84e6cf11bba5563ccff0a81
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-04 21:58:12 +0530

    Added dependency injection for output port and changed methods

--- topologycatalogue/application/src/main/java/dev/catalogue/topology/application/ports/input/NetworkInputPort.java
+++ topologycatalogue/application/src/main/java/dev/catalogue/topology/application/ports/input/NetworkInputPort.java
@@ -1,12 +1,20 @@
 package dev.catalogue.topology.application.ports.input;
+import java.util.function.Predicate;
+
+import javax.inject.Inject;
+
+import dev.catalogue.topology.application.ports.output.RouterOutputPort;
 import dev.catalogue.topology.application.usecases.*;
 
 import dev.catalogue.topology.domain.valueobj.*;
 import dev.catalogue.topology.domain.entity.*;
+import dev.catalogue.topology.domain.service.Networkservice;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class NetworkInputPort implements NetworkUseCase{
+	@Inject
+    RouterOutputPort routerOutputPort;
 	@Override
     public Network createNetwork(
             IP networkAddress, String networkName, int networkCidr) {
@@ -19,12 +27,32 @@ public class NetworkInputPort implements NetworkUseCase{
     }	
 	@Override
     public Switch addNetworkToSwitch(Network network, Switch networkSwitch) {
-        networkSwitch.addNetworkToSwitch(network);
-        return networkSwitch;
+        ID routerId = networkSwitch.getRouterId();
+        ID switchId = networkSwitch.getId();
+        EdgeRouter edgeRouter = (EdgeRouter) routerOutputPort
+                .retrieveRouter(routerId);
+        Switch switchToAddNetwork = edgeRouter
+                .getSwitches()
+                .get(switchId);
+        switchToAddNetwork.addNetworkToSwitch(network);
+        routerOutputPort.persistRouter(edgeRouter);
+        return switchToAddNetwork;
     }
-	@Override
-    public Switch removeNetworkFromSwitch(Network network, Switch networkSwitch) {
-        networkSwitch.removeNetworkFromSwitch(network);
-        return networkSwitch;
-    }
+	 @Override
+	    public Switch removeNetworkFromSwitch(String networkName, Switch networkSwitch) {
+	        ID routerId = networkSwitch.getRouterId();
+	        ID switchId = networkSwitch.getId();
+	        EdgeRouter edgeRouter = (EdgeRouter) routerOutputPort
+	                .retrieveRouter(routerId);
+	        Switch switchToRemoveNetwork = edgeRouter
+	                .getSwitches()
+	                .get(switchId);
+	        Predicate<Network> networkPredicate = Network.getNetworkNamePredicate(networkName);
+	        var network = Networkservice.
+	                findNetwork(switchToRemoveNetwork.getSwitchNetworks(), networkPredicate);
+	        routerOutputPort.persistRouter(edgeRouter);
+	        return switchToRemoveNetwork.removeNetworkFromSwitch(network)
+	                ? switchToRemoveNetwork
+	                : null;
+	    }
 }
--- topologycatalogue/application/src/main/java/dev/catalogue/topology/application/usecases/NetworkUseCase.java
+++ topologycatalogue/application/src/main/java/dev/catalogue/topology/application/usecases/NetworkUseCase.java
@@ -5,5 +5,5 @@ import dev.catalogue.topology.domain.entity.*;
 public interface NetworkUseCase {
 	Network createNetwork(IP networkAddress, String networkName, int networkCidr);
 	Switch addNetworkToSwitch(Network network, Switch networkSwitch);
-	Switch removeNetworkFromSwitch(Network network, Switch networkSwitch);
+	Switch removeNetworkFromSwitch(String name, Switch networkSwitch);
 }
