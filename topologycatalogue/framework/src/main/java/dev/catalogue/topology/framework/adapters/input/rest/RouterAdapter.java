b0e17944bd87d9d94f7f5abea7e3dd54941abb74
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-04 23:21:18 +0530

    Created Router Adapter for Rest services for router operations

---
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/input/rest/RouterAdapter.java
@@ -0,0 +1,43 @@
+package dev.catalogue.topology.framework.adapters.input.rest;
+
+import io.smallrye.mutiny.Uni;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import dev.catalogue.topology.application.usecases.RouterUseCase;
+import dev.catalogue.topology.domain.valueobj.ID;
+
+@ApplicationScoped
+@Path("/router")
+@Tag(name = "Router Operations", description = "Router management operations")
+public class RouterAdapter {
+	@Inject
+    RouterUseCase routerUseCase;
+   
+	@Transactional
+    @GET
+    @Path("/{id}")
+    @Operation(operationId = "retrieveRouter", description = "Retrieve a router from the network catalogue")
+    public Uni<Response> retrieveRouter(@PathParam("id") ID id) {
+        return Uni.createFrom()
+                .item(routerUseCase.retrieveRouter(id))
+                .onItem()
+                .transform(f -> f != null ? Response.ok(f) : Response.ok(null))
+                .onItem()
+                .transform(Response.ResponseBuilder::build);
+    }
+	
+	
+}
