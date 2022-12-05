67067978679a7395e9c9ec859fd6cdeae02ba512
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-05 12:52:35 +0530

    Created Rest endpoint to add switch to and Edge router

--- topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/input/rest/SwitchAdapter.java
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/input/rest/SwitchAdapter.java
@@ -5,7 +5,13 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import dev.catalogue.topology.application.usecases.RouterUseCase;
 import dev.catalogue.topology.application.usecases.SwitchUseCase;
+import dev.catalogue.topology.domain.entity.EdgeRouter;
+import dev.catalogue.topology.domain.entity.Router;
+import dev.catalogue.topology.domain.entity.Switch;
 import dev.catalogue.topology.domain.valueobj.ID;
+import dev.catalogue.topology.domain.valueobj.IP;
+import dev.catalogue.topology.domain.valueobj.Routertype;
+import dev.catalogue.topology.framework.adapters.input.rest.request.SwitchRequest;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -38,4 +44,31 @@ public class SwitchAdapter {
 	                .onItem()
 	                .transform(Response.ResponseBuilder::build);
 	    }
+	    @Transactional
+	    @POST
+	    @Path("/create/{edgeRouterId}")
+	    @Operation(operationId = "createAndAddSwitchToEdgeRouter", description = "Create switch and add to an edge router")
+	    public Uni<Response> createAndAddSwitchToEdgeRouter(
+	            SwitchRequest createSwitch, @PathParam("edgeRouterId") String edgeRouterId
+	    ) {
+	        Switch newSwitch = switchUseCase.
+	                createSwitch(
+	                        createSwitch.getVendor(),
+	                        createSwitch.getModel(),
+	                        IP.fromAddress(createSwitch.getIp()),
+	                        createSwitch.getLocation(),
+	                        createSwitch.getSwitchType());
+	        Router edgeRouter = routerUseCase.retrieveRouter(ID.withId(edgeRouterId));
+	        if(!edgeRouter.getRoutertype().equals(Routertype.Edge))
+	            throw new UnsupportedOperationException("Please inform the id of an edge router to add a switch");
+	        Router router = switchUseCase.addSwitchToEdgeRouter(newSwitch, (EdgeRouter) edgeRouter);
+
+	        return Uni.createFrom()
+	                .item((EdgeRouter) routerUseCase.persistRouter(router))
+	                .onItem()
+	                .transform(f -> f != null ? Response.ok(f) : Response.ok(null))
+	                .onItem()
+	                .transform(Response.ResponseBuilder::build);
+	    }
+
 }
