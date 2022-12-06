acc99995aafa6cf419f9461b1e14f7ffc179ec2a
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-06 15:21:46 +0530

    Created adapter implementation of RouterOutputport for Mysql

---
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/output/mysql/RouterSQLAdapter.java
@@ -0,0 +1,35 @@
+package dev.catalogue.topology.framework.adapters.output.mysql;
+
+import dev.catalogue.topology.framework.adapters.output.mysql.mappers.RouterMapper;
+import dev.catalogue.topology.framework.adapters.output.mysql.respository.*;
+import dev.catalogue.topology.application.ports.output.*;
+import dev.catalogue.topology.domain.entity.Router;
+import dev.catalogue.topology.domain.valueobj.ID;
+import io.quarkus.hibernate.reactive.panache.Panache;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+public class RouterSQLAdapter implements RouterOutputPort {
+        @Inject
+        RouterRepository RouterRepo;
+        @Override
+        public Router retrieveRouter(ID id) {
+            var routerData = RouterRepo.findById(id.getUuid()).subscribe().asCompletionStage().join();
+            return RouterMapper.routerDataToDomain(routerData);
+        }
+        @Override
+        public Router removeRouter(ID id) {
+            var removed = RouterRepo.deleteById(id.getUuid()).subscribe().asCompletionStage().join();
+            if(!removed){
+                throw new InternalError();
+            }
+            return null;
+        }
+        @Override
+        public Router persistRouter(Router router) {
+            var routerData = RouterMapper.routerDomainToData(router);
+            Panache.withTransaction(()->RouterRepo.persist(routerData));
+            return router;
+        }
+
+}
