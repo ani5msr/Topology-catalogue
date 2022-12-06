42c5bc016e1a9887f20b63b9b19762826e7162f9
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-06 15:24:57 +0530

    Created adapter implementation of SwitchOutputport for Mysql

---
+++ topologycatalogue/framework/src/main/java/dev/catalogue/topology/framework/adapters/output/mysql/SwitchSQLAdapter.java
@@ -0,0 +1,21 @@
+package dev.catalogue.topology.framework.adapters.output.mysql;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import dev.catalogue.topology.application.ports.output.SwitchOutputPort;
+import dev.catalogue.topology.domain.entity.Switch;
+import dev.catalogue.topology.domain.valueobj.ID;
+import dev.catalogue.topology.framework.adapters.output.mysql.mappers.RouterMapper;
+import dev.catalogue.topology.framework.adapters.output.mysql.respository.SwitchRepository;
+public class SwitchSQLAdapter implements SwitchOutputPort {
+	@Inject
+    SwitchRepository SwitchRepo;
+
+    @Override
+    public Switch retrieveSwitch(ID id) {
+        var switchData = SwitchRepo.findById(id.getUuid()).subscribe().asCompletionStage().join();
+        return RouterMapper.switchDataToDomain(switchData);
+    }
+
+}
