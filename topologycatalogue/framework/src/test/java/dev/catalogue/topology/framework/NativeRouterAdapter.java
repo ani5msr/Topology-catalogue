2a2c4833cea613d2ae61930ae0fc38b85ffbfe0c
Anirudh Mishra <anirudh5msr@gmail.com>
2022-12-11 20:08:17 +0530

    Added Native image test for Router

---
+++ topologycatalogue/framework/src/test/java/dev/catalogue/topology/framework/NativeRouterAdapter.java
@@ -0,0 +1,6 @@
+package dev.catalogue.topology.framework;
+import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+public class NativeRouterAdapter extends RouterAdapterTest {
+
+}
